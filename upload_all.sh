2183f032c53e0ac0e28152efd3865e2d3e3eccae
Colin Shea <colin@evaryont.me>
2014-12-29 17:30:39 -0700

    Nodes dir, a local clone

--- upload_all.sh
+++ upload_all.sh
@@ -5,6 +5,16 @@ set -euo pipefail
 
 berks -q && berks upload -q
 
-for i in roles/*.json; do
-  knife role from file $i
-done
+roles_diff_lc=$(knife diff roles | wc -l)
+if [[ $roles_diff_lc > 0 ]]; then
+  for i in roles/*.json; do
+    knife role from file $i >/dev/null
+  done
+fi
+
+nodes_diff_lc=$(knife diff nodes | wc -l)
+if [[ $nodes_diff_lc > 0 ]]; then
+  for i in nodes/*.json; do
+    knife node from file $i >/dev/null
+  done
+fi
