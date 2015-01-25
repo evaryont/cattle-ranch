412ddb9659cc191c6cd5f761035601e6f14e10d2
Colin Shea <colin.shea@evogi.com>
2015-01-24 18:22:50 -0700

    version bump ranchhand for sshd recipe

--- cookbooks/ranchhand/metadata.rb
+++ cookbooks/ranchhand/metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'colin@shea.at'
 license          'all_rights'
 description      'Helper cookbook for cattle-ranch'
 long_description 'Various helper recipes for my private chef repository'
-version          '0.3.10'
+version          '0.4.0'
 
 depends 'sshd'
 
