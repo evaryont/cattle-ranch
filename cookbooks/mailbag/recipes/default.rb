4aee01881e68a6d9570eb24bb4b005ae30d8c9b8
Colin Shea <colin.shea@evogi.com>
2015-04-03 00:01:44 -0700

    mailbag encrypted fs image

--- cookbooks/mailbag/recipes/default.rb
+++ cookbooks/mailbag/recipes/default.rb
@@ -1,5 +1,19 @@
+# create file for loop block device
+file '/mailbag.img' do
+  owner "root"
+  group "root"
+  mode "00600"
+  action :create_if_missing
+  notifies :run, "execute[set-file-size]", :immediately
+end
+# Set file size for loop file, 7GB file
+execute "set-file-size" do
+  command "/usr/bin/truncate -s 7168M /mailbag.img"
+  action :nothing
+end
+
 encrypted_volume "/encrypted" do
-  volume  "/raw_test_volume.img"
+  volume  "/mailbag.img"
   fstype  "ext3"
 end
 
