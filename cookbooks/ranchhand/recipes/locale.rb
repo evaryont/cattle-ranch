58cbe5b42291d25ad55fe938dc390551377f3af7
Colin Shea <colin.shea@evogi.com>
2015-05-25 01:10:24 -0700

    Split the locale setup into a wrapper recipe
    Need to create locale.gen file so that the nodes do have the en_US locale
    configured. (And later generated by the locale cookbook.)

---
+++ cookbooks/ranchhand/recipes/locale.rb
@@ -0,0 +1,16 @@
+node.default['locale']['lang'] = 'en_US.UTF-8'
+node.default['locale']['lc_all'] = 'en_US.UTF-8'
+
+file '/etc/locale.gen' do
+  content <<-EOLOCALE
+# #{node['config_disclaimer']}
+
+en_US.UTF-8 UTF-8
+en_US ISO-8859-1
+EOLOCALE
+  owner 'root'
+  group 'root'
+  mode '0644'
+end
+
+include_recipe 'recipe[locale::default]'
