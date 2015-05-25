ebe81ffe5bc77d07e96793298d7b25fe51cbbdf0
Colin Shea <colin.shea@evogi.com>
2015-05-25 16:13:29 -0700

    pass an array for components

--- cookbooks/ranchhand/metadata.rb
+++ cookbooks/ranchhand/metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'colin@shea.at'
 license          'all_rights'
 description      'Helper cookbook for cattle-ranch'
 long_description 'Various helper recipes for my private chef repository'
-version          '0.5.8'
+version          '0.5.9'
 
 depends 'sshd'
 depends 'iptables-ng'
--- cookbooks/ranchhand/recipes/backports.rb
+++ cookbooks/ranchhand/recipes/backports.rb
@@ -2,6 +2,6 @@ if %w(jessie wheezy).include? node['lsb']['codename']
   apt_repository "#{node['lsb']['codename']}-backports" do
     uri          'http://http.debian.net/debian'
     distribution "#{node['lsb']['codename']}-backports"
-    components   'main'
+    components   [ 'main' ]
   end
 end
