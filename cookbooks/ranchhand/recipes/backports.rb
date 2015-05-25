7d196bbaee88a0a0cb6b79c0d9ec4e619706d77f
Colin Shea <colin.shea@evogi.com>
2015-05-25 16:09:30 -0700

    add backports to debian servers

---
+++ cookbooks/ranchhand/recipes/backports.rb
@@ -0,0 +1,7 @@
+if %w(jessie wheezy).include? node['lsb']['codename']
+  apt_repository "#{node['lsb']['codename']}-backports" do
+    uri          'http://http.debian.net/debian'
+    distribution "#{node['lsb']['codename']}-backports"
+    components   'main'
+  end
+end
