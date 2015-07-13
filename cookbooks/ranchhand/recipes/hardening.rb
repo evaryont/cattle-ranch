65dd53dc14227106b2d8adf4089efb2bd92bf425
Colin Shea <colin@evaryont.me>
2015-07-13 02:23:26 +0000

    use the correct name for the resource
    my previous shenanigans was incorrect

--- cookbooks/ranchhand/recipes/hardening.rb
+++ cookbooks/ranchhand/recipes/hardening.rb
@@ -3,14 +3,10 @@ directory '/etc/security/limits.d' do
   group 'root'
   mode '0755'
 end
-
-if arch?
-  # include this early so that unwind can find the resource
-  include_recipe 'os-hardening::pam'
-  # pam_ccreds is an AUR package, which won't be detected if it's not already
-  # installed.
-  unwind 'package[pam_ccreds]'
-end
-
 include_recipe 'os-hardening::default'
 
+if arch?
+  # pam_ccreds is an AUR package, which won't be detected if it's not already
+  # installed. Don't worry about the package anyways on arch.
+  unwind 'package[pam-ccreds]'
+end
