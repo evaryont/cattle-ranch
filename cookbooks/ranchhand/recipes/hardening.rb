ca24c8245b241711fca40d196f1c58006e1beda0
Colin Shea <colin@evaryont.me>
2015-07-18 09:47:34 +0000

    Include hardening recipes myself
    This is all of them, as of the current release. EXCEPT sysctl, since I manage
    that one on my own.

--- cookbooks/ranchhand/recipes/hardening.rb
+++ cookbooks/ranchhand/recipes/hardening.rb
@@ -3,7 +3,15 @@ directory '/etc/security/limits.d' do
   group 'root'
   mode '0755'
 end
-include_recipe 'os-hardening::default'
+
+include_recipe 'os-hardening::packages'
+include_recipe 'os-hardening::limits'
+include_recipe 'os-hardening::login_defs'
+include_recipe 'os-hardening::minimize_access'
+include_recipe 'os-hardening::pam'
+include_recipe 'os-hardening::profile'
+include_recipe 'os-hardening::securetty'
+include_recipe 'os-hardening::suid_sgid' if node['security']['suid_sgid']['enforce']
 
 if arch?
   # pam_ccreds is an AUR package, which won't be detected if it's not already
