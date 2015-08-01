ba792bfedabfdf557a984c4e2e8e7f39a347c205
Colin Shea <colin@evaryont.me>
2015-08-01 03:22:11 +0000

    Skip openssl headers package on N/A nodes

--- cookbooks/ranchhand/recipes/chef_vault.rb
+++ cookbooks/ranchhand/recipes/chef_vault.rb
@@ -1,10 +1,18 @@
+# Install openssl headers on the system, but only do so on detected platforms.
+# (See attributes/default.rb) If there isn't a detected library to install (like
+# on Arch), skip this step.
 package 'openssl headers' do
   package_name node['ranchhand']['openssl_dev']
-end.run_action(:install)
+end.run_action(:install) if node['ranchhand']['openssl_dev']
 
+# Install the cheffish gem since it's useful to manage resources related to the
+# chef server itself (like data bags!)
 chef_gem 'cheffish'
 require 'cheffish'
 
+# Ensure chef-vault is installed on all the nodes
 include_recipe 'chef-vault::default'
 
+# This is a data bag that will be used to store SSL certs, to be used for HTTPS,
+# STARTTLS in SMTP, and others.
 chef_data_bag 'ssl'
