fd091d548d258a5da77cdcd3e4db68e2f98729e0
Colin Shea <colin@evaryont.me>
2015-08-06 23:07:06 +0000

    Manage the dhparam file, update the nginx template

--- cookbooks/ranchhand/recipes/ssl_certs.rb
+++ cookbooks/ranchhand/recipes/ssl_certs.rb
@@ -20,3 +20,11 @@ execute 'generate-dhparam' do
   command "openssl dhparam -out #{node['ranchhand']['ssl_cert_dir']}/dhparam.pem 2048"
   creates "#{node['ranchhand']['ssl_cert_dir']}/dhparam.pem"
 end
+
+# Create a resource to manage this file, both to ensure it's mode/owner/etc, but
+# also so that Chef won't treat the file as unknown.
+file "#{node['ranchhand']['ssl_cert_dir']}/dhparam.pem" do
+  owner 'root'
+  group 'root'
+  mode '0644'
+end
