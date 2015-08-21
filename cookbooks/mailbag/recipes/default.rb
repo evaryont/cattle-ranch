82d9fd2824ce71b667e7901b1865d25a4c0a3bab
Colin Shea <colin@evaryont.me>
2015-08-21 23:30:03 +0000

    ensure the directory to mount the mailbag exists

--- cookbooks/mailbag/recipes/default.rb
+++ cookbooks/mailbag/recipes/default.rb
@@ -1,6 +1,10 @@
 include_recipe 'lvm'
 include_recipe 'filesystem'
 
+# This is where mail will be stored
+directory '/opt/mailbag'
+
+# Create an encrypted filesystem that will store the mail
 encrypted_blockdevice 'datacrypt' do
   size 7168
   keystore 'encrypted_databag'
@@ -9,10 +13,11 @@ end
 filesystem 'datacrypt' do
   size '7168'
   fstype 'ext4' 
-  mount '/opt/mailbag' # where to mount the encrypted filesystem onto the host
+  mount '/opt/mailbag'
   file '/var/mailbag.fs' # location of where the encrypted filesystem image will go
   device '/dev/loop7' # not sure what this actually does...
 end
 
+# Setup the servers
 include_recipe 'mailbag::smtp'
 include_recipe 'mailbag::imap'
