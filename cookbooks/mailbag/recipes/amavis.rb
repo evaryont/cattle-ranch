cf5b0a4d17d1bc5834665272011a624b6c291411
Colin Shea <colin@evaryont.me>
2015-09-14 01:33:01 +0000

    disable clamd

--- cookbooks/mailbag/recipes/amavis.rb
+++ cookbooks/mailbag/recipes/amavis.rb
@@ -1,7 +1,7 @@
 node.default['spamassassin']['conf']['required_score'] = 5
 include_recipe 'onddo-spamassassin::default'
 
-node.default["clamav"]["clamd"]["enabled"] = true
+node.default["clamav"]["clamd"]["enabled"] = false
 node.default["clamav"]["freshclam"]["enabled"] = true
 include_recipe 'clamav::default'
 
