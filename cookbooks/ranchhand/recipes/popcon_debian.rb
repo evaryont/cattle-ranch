1658ecb6f8b05dcd3a51fea062395eb21b4fa6ec
Colin Shea <colin@evaryont.me>
2015-10-08 06:22:41 +0000

    Fix HOSTID variable for debian popularity content

--- cookbooks/ranchhand/recipes/popcon_debian.rb
+++ cookbooks/ranchhand/recipes/popcon_debian.rb
@@ -11,8 +11,8 @@ submission_url = "http://popcon.debian.org/cgi-bin/popcon.cgi"
 file "/etc/popularity-contest.conf" do
   content <<-EOPOPCON
 PARTICIPATE="yes"
-MY_ID="#{Digest::MD5.hexdigest(node["keys"]["ssh"]["host_rsa_public"] || '')}"
-SUBMITURLS=#{submission_url}
+MY_HOSTID="#{Digest::MD5.hexdigest(node["keys"]["ssh"]["host_rsa_public"] || '')}"
+SUBMITURLS="#{submission_url}"
 USE_HTTP="yes"
 # Needs a modern popcon client & gpg installed
 ENCRYPT="maybe"
